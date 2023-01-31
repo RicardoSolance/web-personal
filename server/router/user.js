381db575f9e38b3c93de425d1ce0cee8eb00d00a
unknown <ricardontutumu@gmail.com>
2023-01-31 16:28:55 +0100

    Gettin Users

--- server/router/user.js
+++ server/router/user.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const UserController = require("../controllers/user");
+const User = require("../controllers/user");
 const user = require("../middlewares/authenticated");
 
 const api = express.Router();
 
-api.get("/user/me", [user.isAuthenticated], UserController.getMe);
+api.get("/user/me", [user.isAuthenticated], User.getMe);
+api.get("/user/users", [user.isAuthenticated], User.getUsers);
+api.post("/user/users", [user.isAuthenticated], User.createUser);
 
 module.exports = api;
