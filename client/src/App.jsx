5d6ed8b2063521d347d3c04ec898516a93cf4022
unknown <ricardontutumu@gmail.com>
2023-02-06 21:19:58 +0100

    Client

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { Button } from 'semantic-ui-react'
+
+
+function App() {
+
+
+  return (
+   <div>
+    <h1>Inicializando mi web personal FRONT</h1>
+    <Button content='Primary' primary />
+   </div>
+  )
+}
+
+export default App
